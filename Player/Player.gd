ebab98bc3398fb97bb722e7a4d51c19572548f9e
AryanSanghi159 <56901135+AryanSanghi159@users.noreply.github.com>
2021-02-16 12:28:13 -0800

    saved the game

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,11 +6,12 @@ var velocity = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var Ray = $RayCast2D
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
-	input_vector.y = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up"))
+	input_vector.x = (Input.get_action_strength("right") - Input.get_action_strength("left"))
+	input_vector.y = (Input.get_action_strength("down") - Input.get_action_strength("up"))
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
@@ -24,3 +25,4 @@ func _physics_process(delta):
 	
 	velocity = velocity * MAX_SPEED
 	move_and_slide(velocity)
+
